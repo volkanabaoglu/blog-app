e4bea7189b9f43d0efc679b053c5134aade8ef80
Volkan Abaoglu <volkan.abaoglu>
2024-03-20 22:04:18 +0300

    Refactor import statements and fix formatting in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
-import HomePage from './components/HomePage'
-import BlogListPage from './components/BlogListPage.js';
-import BlogDetailsPage from './components/BlogDetailPage';
-import ContactPage from './components/ContactPage';
-import NotFoundPage from './components/NotFoundPage';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import HomePage from "./components/HomePage";
+import BlogListPage from "./components/BlogListPage.js";
+import BlogDetailsPage from "./components/BlogDetailPage";
+import ContactPage from "./components/ContactPage";
+import NotFoundPage from "./components/NotFoundPage";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const router = createBrowserRouter([
   {
@@ -20,21 +16,21 @@ const router = createBrowserRouter([
     element: <HomePage />,
   },
   {
-    path: "/blogs" ,
-    element : <BlogListPage />
+    path: "/blogs",
+    element: <BlogListPage />,
   },
   {
-    path: "/blogs/:id" ,
-    element : <BlogDetailsPage />
+    path: "/blogs/:id",
+    element: <BlogDetailsPage />,
   },
   {
-    path: "/contact" ,
-    element : <ContactPage />
+    path: "/contact",
+    element: <ContactPage />,
   },
   {
-    path: "" ,
-    element : <NotFoundPage />
-  }
+    path: "",
+    element: <NotFoundPage />,
+  },
 ]);
 
 root.render(
