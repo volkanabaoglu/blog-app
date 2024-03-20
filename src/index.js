3055a3a18a020553020e9433ac0bce001357b151
Volkan Abaoglu <volkan.abaoglu>
2024-03-20 21:56:36 +0300

    Refactor routing in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import HomePage from './components/HomePage'
+import BlogListPage from './components/BlogListPage.js';
+import BlogDetailsPage from './components/BlogDetailPage';
+import ContactPage from './components/ContactPage';
+import NotFoundPage from './components/NotFoundPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/blogs" ,
+    element : <BlogListPage />
+  },
+  {
+    path: "/blogs/:id" ,
+    element : <BlogDetailsPage />
+  },
+  {
+    path: "/contact" ,
+    element : <ContactPage />
+  },
+  {
+    path: "" ,
+    element : <NotFoundPage />
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
